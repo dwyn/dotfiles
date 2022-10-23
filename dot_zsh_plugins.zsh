67ca4ddff6285f55929ca2201effe84099f295d1
dwieb <10413171+dwyn@users.noreply.github.com>
2022-10-23 16:15:07 -0500

    okhi

---
+++ dot_zsh_plugins.zsh
@@ -0,0 +1,7 @@
+fpath+=( /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+source /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt )
+source /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt/async.zsh
+source /Users/dharmon/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-spaceship-prompt-SLASH-spaceship-prompt/spaceship.zsh
